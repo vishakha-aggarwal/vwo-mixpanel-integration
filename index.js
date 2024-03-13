7247f107c54cef0723a767e2d2e6dc07f096494e
Vishakha Aggarwal <76397035+vishakha-aggarwal@users.noreply.github.com>
2024-03-13 13:53:06 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function VWOMixpanelPlugin(mixpanel){
     window.VWO = window.VWO || [];
+
+    if(!mixpanel.track || !mixpanel.people) {
+        console.warn("VWO Mixpanel Plugin Log - mixpanel is not defined");
+        return;
+    }
+
     var _vis_data = {};
     
     window.VWO.push([
@@ -11,23 +17,19 @@ function VWOMixpanelPlugin(mixpanel){
             if (expId && variationId &&
                 ["VISUAL_AB", "VISUAL", "SPLIT_URL"].indexOf(window._vwo_exp[expId].type) > -1
             ) {
-                if (mixpanel) {
-                    _vis_data["VWO-Test-ID-" + expId] = window._vwo_exp[expId].comb_n[variationId];
-                    _vis_data["experiments"] = _vis_data["experiments"] || [];
-                    _vis_data["experiments"].push({"eN": window._vwo_exp[expId].name, "vN": window._vwo_exp[expId].comb_n[variationId]});
+                _vis_data["VWO-Test-ID-" + expId] = window._vwo_exp[expId].comb_n[variationId];
+                _vis_data["experiments"] = _vis_data["experiments"] || [];
+                _vis_data["experiments"].push({"eN": window._vwo_exp[expId].name, "vN": window._vwo_exp[expId].comb_n[variationId]});
 
-                    mixpanel.track("VWO", _vis_data);
-                    for (var i = 0; i < _vis_data["experiments"].length; i++) {
-                        var experiment = _vis_data["experiments"][i];
-                        mixpanel.track("Experiment Started", {"Experiment name": experiment.eN, "Variant name": experiment.vN});
-                    }
-
-                    mixpanel.people.set({
-                        $vwo_user_id: window.VWO.data.vin.uuid
-                    });
-                } else {
-                    console.warn("VWO Mixpanel Plugin Log - mixpanel is not defined")
+                mixpanel.track("VWO", _vis_data);
+                for (var i = 0; i < _vis_data["experiments"].length; i++) {
+                    var experiment = _vis_data["experiments"][i];
+                    mixpanel.track("Experiment Started", {"Experiment name": experiment.eN, "Variant name": experiment.vN});
                 }
+
+                mixpanel.people.set({
+                    $vwo_user_id: window.VWO.data.vin.uuid
+                });
             }
         },
     ])
